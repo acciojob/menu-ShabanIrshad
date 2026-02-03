99be66307dfaa141c7b1ba61ad0e952dc0a5d0e8
Shaban Irshad <shabanirshad408@gmail.com>
2026-02-03 11:18:22 +0530

    added

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,67 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from 'react';
+import Menu from './Menu';
+// import '../styles/styles.css';
 
+const App=()=>{
+    const dishesh=[
+        {
+            name:'Halwa Puri',
+            category:'breakfast',
+            image:'https://www.tasteatlas.com/images/dishes/a115b8b6115f4ea580599eb24a369408.jpg',
+            price:1299,
+            desc:'atest Hindi Video Song of 2019 "Kithe Reh Gaya" ,In the voice of "Neeti Mohan", composed by "" and the lyrics of this new song are penned by " Kumaar ". Enjoy and stay connected with us'
+        },
+        {
+            name:'Dal Makhni',
+            category:'lunch',
+            image:'https://www.sharmispassions.com/wp-content/uploads/2012/05/dal-makhani7.jpg',
+            price:999,
+            desc:'atest Hindi Video Song of 2019 "Kithe Reh Gaya" ,In the voice of "Neeti Mohan", composed by "" and the lyrics of this new song are penned by " Kumaar ". Enjoy and stay connected with us'
+        },
+        {
+            name:'Lassi Punjabi',
+            category:'shakes',
+            image:'https://www.sharmispassions.com/wp-content/uploads/2023/08/sweet-lassi3.jpg',
+            price:499,
+            desc:'atest Hindi Video Song of 2019 "Kithe Reh Gaya" ,In the voice of "Neeti Mohan", composed by "" and the lyrics of this new song are penned by " Kumaar ". Enjoy and stay connected with us'
+        },
+        {
+            name:'Aloo Paratha',
+            category:'breakfast',
+            image:'https://i0.wp.com/smithakalluraya.com/wp-content/uploads/2015/02/image.1024x1024-95.jpg?resize=720%2C972&ssl=1',
+            price:599,
+            desc:'atest Hindi Video Song of 2019 "Kithe Reh Gaya" ,In the voice of "Neeti Mohan", composed by "" and the lyrics of this new song are penned by " Kumaar ". Enjoy and stay connected with us'
+        },
+        {
+            name:'Chicken 65',
+            category:'lunch',
+            image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaULrDzejMQMqWTNqOPk1SmuBZ2Q_8C9jmAw&s',
+            price:1500,
+            desc:'atest Hindi Video Song of 2019 "Kithe Reh Gaya" ,In the voice of "Neeti Mohan", composed by "" and the lyrics of this new song are penned by " Kumaar ". Enjoy and stay connected with us'
+        },
+        {
+            name:'Mojito Continental',
+            category:'shakes',
+            image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQ2A7qow7FdQkM3jcINiNG9vNvIuJyY01Ovg&s',
+            price:299,
+            desc:'atest Hindi Video Song of 2019 "Kithe Reh Gaya" ,In the voice of "Neeti Mohan", composed by "" and the lyrics of this new song are penned by " Kumaar ". Enjoy and stay connected with us'
+        },
+        {
+            name:'Dum Aloo',
+            category:'lunch',
+            image:'https://myspicetrunk.com/wp-content/uploads/2021/04/20210410_1838302-e1618165098812.jpg?v=1618099124',
+            price:1200,
+            desc:'atest Hindi Video Song of 2019 "Kithe Reh Gaya" ,In the voice of "Neeti Mohan", composed by "" and the lyrics of this new song are penned by " Kumaar ". Enjoy and stay connected with us'
+        }
+    ]
+    const [dishArray,setDishArray]=useState([...dishesh]);
+    // console.log(dishesh);
+    return (
+        <>
+        <div id='main'>
+            <Menu data={{dishArray,setDishArray}}/>
+        </div>
+        </>
+    );
+}
+export default App;
---
+++ src/components/Menu.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Menu=({data})=>{
+    const {dishArray,setDishArray}=data;
+    // console.log(dishArray);
+    const cat=dishArray.map((dish)=>dish.category);
+    const categoryData=new Set([...cat]);
+    const category=[...categoryData];
+
+    const handleDish=(dish)=>{
+        console.log(dish);
+        console.log(dishArray);
+        const arr=dishArray.filter( (item) => item.category === dish);
+        setDishArray([...arr]);
+        // console.log(arr);
+    }
+    return (
+        <>
+        <h1 className='heading'>Our Menu</h1>
+        <hr className='underline'></hr>
+        <div className='dish-category'>
+            {category.map((dish,index)=><button key={index} onClick={()=>handleDish(dish)}>{dish}</button>)}
+        </div>
+        <div className='dish-items'>
+            {dishArray.map((dish,index)=>
+                <div key={index} className='dish-item'>
+                    <img src={dish.image} alt='image'/>
+                    <div>
+                        <h4>{dish.name}</h4>
+                        <b><small>{dish.price}</small></b>
+                        <hr></hr>
+                        <p>{dish.desc}</p>
+                    </div>    
+                </div>
+            )}
+
+        </div>
+        
+
+        </>
+    );
+}
+export default Menu;
